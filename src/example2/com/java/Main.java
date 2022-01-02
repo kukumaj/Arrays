ddc76d5cea90a7ec70c5ab48a85c2c72ccbc6a13
Ryszard <zakumaj@gmail.com>
2022-01-02 22:01:15 +0100

    poprawki

--- src/example2/com/java/Main.java
+++ src/example2/com/java/Main.java
@@ -13,7 +13,17 @@ public class Main {
         String[] colors = {"RED","BLUE","WHITE","ORANGE","GREEN","BROWN","PINK","YELLOW"};
 
         Arrays.stream(colors).sorted().forEachOrdered(System.out::println);
+<<<<<<< HEAD
     }
     ClothingItem[] items = new ClothingItem[3];
     items[0] = new ClothingItem("Shirt","L","15");
+=======
+        ClothingItem[] items = new ClothingItem[3];
+        items[0] = new ClothingItem("Shirt","L",15);
+        items[1] = new ClothingItem("Pants","32",20);
+        items[2] = new ClothingItem("Hat","L",12);
+        Arrays.stream(items).forEach(System.out::println);
+    }
+
+>>>>>>> d20cdc3 (poprawki)
 }
