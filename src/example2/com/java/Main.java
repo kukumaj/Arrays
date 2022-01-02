4760a12b435dd11f1e0ff5f16c7a9593a55a88a7
Ryszard <zakumaj@gmail.com>
2022-01-02 22:46:26 +0100

    Ostateczna:)

--- src/example2/com/java/Main.java
+++ src/example2/com/java/Main.java
@@ -7,26 +7,26 @@ import java.util.Arrays;
 public class Main<fore, foreach> {
     public static <copied> void main(String[] args) {
         System.out.println("Array of primitives ");
-        int[]  ints ={3,6,9,1};
+        int[] ints = {3, 6, 9, 1};
         Arrays.stream(ints).forEach(System.out::println);
         System.out.println("Array of strings ");
-        String[] colors = {"RED","BLUE","WHITE","ORANGE","GREEN","BROWN","PINK","YELLOW"};
+        String[] colors = {"RED", "BLUE", "WHITE", "ORANGE", "GREEN", "BROWN", "PINK", "YELLOW"};
 
         Arrays.stream(colors).sorted().forEachOrdered(System.out::println);
 
         ClothingItem[] items = new ClothingItem[3];
-        items[0] = new ClothingItem("Shirt","L",15);
-        items[1] = new ClothingItem("Pants","32",20);
-        items[2] = new ClothingItem("Hat","L",12);
-        for (ClothingItem item:items) {
-            System.out.println(item);
-    }
-        ClothingItem[] copied = Arrays.copyOf(items,items.length) ;
-        for (ClothingItem item:copied) {
+        items[0] = new ClothingItem("Shirt", "L", 15);
+        items[1] = new ClothingItem("Pants", "32", 20);
+        items[2] = new ClothingItem("Hat", "L", 12);
+        for (ClothingItem item : items) {
             System.out.println(item);
         }
+        ClothingItem[] copied = Arrays.copyOf(items, items.length);
+        for (ClothingItem item : copied) {
+            System.out.println(item);
         }
     }
+}
 
 
 
